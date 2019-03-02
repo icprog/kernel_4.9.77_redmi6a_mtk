fa4ff63bd14480cb81d960468119873d2b29b17f
Sridhar Ananthakrishnan <sridhar@aifuse.com>
2019-03-02 23:55:42 +0000

    Initial Commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#0 SMP PREEMPT Sat Mar 2 22:35:02 UTC 2019"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "docker-s-1vcpu-1gb-blr1-01"
+#define LINUX_COMPILER "gcc version 5.5.0 (Linaro GCC 5.5-2017.10) "
